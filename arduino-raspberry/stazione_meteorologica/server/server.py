120cae4e5e7ccc3339ba7f3658b60874984304b1
Gabriele Ferrero <63954224+GabrieleFerrero@users.noreply.github.com>
2022-01-04 20:08:02 +0100

    Update server.py

--- arduino-raspberry/stazione_meteorologica/server/server.py
+++ arduino-raspberry/stazione_meteorologica/server/server.py
@@ -460,6 +460,8 @@ class EliminazioneDatiVecchi(threading.Thread):
                     try: 
                         errore = False    
                         
+                        data_ora_corrente = str(data_ora_corrente).replace(":", "")  
+                        
                         # rinominazione database
                         subprocess.run(["mv", f"{dir_path}/database/dati_sensori_stazioni.db", f"{dir_path}/database/{data_ora_corrente}.db"])
 
